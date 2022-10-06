670f757507fae6cf6c32e995d8c25a94c26a1873
Chris <97849372+TheChrisIsHere@users.noreply.github.com>
2022-10-06 13:51:38 +0530

    Update keepAlive.js

--- util/keepAlive.js
+++ util/keepAlive.js
@@ -1,12 +1,12 @@
+require('dotenv').cofig();
 function keepAlive()
   {
     const express = require('express');
     const app = express();
-    const port = process.env.PORT || 5000;
     
     app.get('/', (req, res) => res.send('Bot Is Online!!'));
     
-    app.listen(port, () =>
+    app.listen(process.env.PORT, () =>
         console.log(`Your app is listening to http://localhost:${port}`)
     ); 
   }
